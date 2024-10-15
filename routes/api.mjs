dee6779d9ab98836ad21c34333391a7591b5d7c3
LenzCompany <ndaacy@gmail.com>
2024-10-15 08:56:01 +0000

    add islamic menu and games!

--- routes/api.mjs
+++ routes/api.mjs
@@ -6,6 +6,11 @@ const creator = "Ndaa"
 /*
 * @Pesan Error
 */
+
+async function getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
 const loghandler = {
     notparam: {
         status: false,
@@ -137,12 +142,176 @@ app.get('/openai', async (req, res, next) => {
     let result = await json.json()
     res.json(result)
 })
-//downloader
+
+// ------------------> Downloader <------------------------------
+
 app.get('/downloader/tiktok', async (req, res, next) => {
     const url = req.query.url;
     if(!url) return res.json(loghandler.noturl)
        let json = await tiktok.tiktokdl(url)
     res.json(json)
+})
+
+// --------------------------------> GAMES <------------------------------------
+
+app.get('/games/asahotak', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/asahotak.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/caklontong', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/caklontong.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/family100', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/family100.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/siapakahaku', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/siapakahaku.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/susunkata', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/susunkata.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakbendera', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakbendera.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakgambar', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakgambar.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakkabupaten', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakkabupaten.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakkalimat', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakkalimat.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakkata', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakkata.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebakkimia', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebakkimia.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebaklirik', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebaklirik.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tebaktebakan', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tebaktebakan.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/games/tekateki', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/BochilTeam/database/refs/heads/master/games/tekateki.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+
+// ------------------------> ISLAMIC <---------------------------
+app.get('/islam/abdul-somad', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/ceramah/abdul%20somad.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/adi-hidayat', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/ceramah/adi%20hidayat.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/felix-siauw', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/ceramah/felix%20siauw.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/khalid-basalamah', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/ceramah/khalid%20basalamah.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+//BATAS 
+app.get('/islam/asmaulhusna', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/data/dataAsmaulHusna.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/ayatkursi', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/data/dataAyatKursi.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/bacaanshalat', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/data/dataBacaanShalat.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/doaharian', async (req, res, next) => {
+    let json = await fetch(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/data/dataDoaHarian.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/kisahnabi', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/data/dataKisahNabi.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/niatshalat', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/data/dataNiatShalat.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/tahlil', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/data/dataTahlil.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
+})
+app.get('/islam/wirid', async (req, res, next) => {
+    let json = await fetch(`https://github.com/zeeoneofficial/My-SQL-Results/raw/refs/heads/master/data/dataWirid.json`)
+ let result = await json.json()
+ const reslt = result
+ res.json(reslt)
 })
 //nsfw
 app.get('/nsfw/waifu', async (req, res, next) => {
